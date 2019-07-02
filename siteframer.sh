883679b68c7cc00601c4c1d130a2fcd448de7d55
scorpiorooster <tlhand3@gmail.com>
2019-07-02 08:37:07 -0600

    Update siteframer.sh

--- siteframer.sh
+++ siteframer.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+# BINDtoDir
 # REQUEST BIND TXT FILE
 while true; do
-printf "\n\n\t";read -p "Enter BIND .TXT file:	" BINDFILE; printf "\n\n"
+printf "\n\n\t";read -p "Enter BIND .TXT file :  " BINDFILE; printf "\n\n"
 		case $BINDFILE in
 			*txt ) printf "\n\nContinuing...\n\n"; break;;
 			* ) printf "Enter a valid .txt file";;
@@ -10,9 +11,10 @@ printf "\n\n\t";read -p "Enter BIND .TXT file:	" BINDFILE; printf "\n\n"
 grep -Eo "(^\w+[.]+\w+[.]+\w+[.])" $BINDFILE | sed 's/.$//' | column -x
 grep -Eo "(^\w+[.]+\w+[.]+\w+[.])" $BINDFILE | sed 's/.$//' > hostlist.txt
 printf "\n\n\n\tThis has been saved as hostlist.txt\n\n\n"
+# USER CONFIRMATION FOR LIST OF HOSTS
 while true; do
-	read -p "Continue using this list? (Y/N) :" yn;
-		case $yn in
+	read -p "Continue using this list? (Y/N) :  " YN;
+		case $YN in
 			[Yy]* ) printf "\n\nContinuing...\n\n"; break;;
 			[Nn]* ) printf "\nExiting...\n\n"; return;;
 			* ) printf "Answer yes or no.\n";;
@@ -32,30 +34,33 @@ rm indextemp.html; cat >> indextemp.html << 'EOF'
 </html>
 EOF
 INDEXTEMP='indextemp.html'; cat indextemp.html
-printf "\n\n\n\tThis has been saved as indextemp.html\n"
-printf "\ttitle and h1 will be replaced with server name.\n\n"
+printf "\n\n\tThis has been saved as indextemp.html"
+printf "\n\t<title> and <h1> will be replaced with server name.\n\t"
+# USER CONFIRMATION FOR INDEX TEMPLATE
 while true; do
-	read -p "Continue using this template? (Y/N) :" yn
-		case $yn in
+	read -p "Continue using this template? (Y/N) :  " YN
+		case $YN in
 			[Yy]* ) echo "Continuing..."; break;;
 			[Nn]* ) echo "Exiting..."; return;;
 			* ) echo "Please answer yes or no.";;
 		esac; done
-read -p "Enter sites directory (sites-available):  " SITESDIR;
+read -p "Enter webroots directory (sites-available) :  " SITESDIR;
 printf "\n\n\tYou have entered:  ${SITESDIR}\n\n"
+# USER CONFIRMATION FOR SITES DIRECTORY
 while true; do
-	read -p "Continue using this directory? (Y/N) :" yn
-		case $yn in
+	read -p "Continue using this directory? (Y/N) :  " YN
+		case $YN in
 			[Yy]* ) echo "Continuing..."; break;;
 			[Nn]* ) echo "Exiting..."; return;;
 			* ) echo "Please answer yes or no.";;
 		esac; done
-# MAKE NAMED index.html
 while read SERVERNAME; do
-	# MAKE SERVER DIRECTORIES FROM .hostlist.txt
+	# CREATE ROOT SERVER DIRECTORIES FROM .hostlist.txt
     mkdir "$SITESDIR/$SERVERNAME"
+	# CREATE index.html IN EACH SERVER ROOT
 	cp $INDEXTEMP $SITESDIR/$SERVERNAME/index.html
+	# RENAME index <title> AND <h1> DATA FOR SITE
 	sed -i "s/titleTEMPLATE/${SERVERNAME}/g" $SITESDIR/$SERVERNAME/index.html
 	sed -i "s/h1TEMPLATE/${SERVERNAME}/g" $SITESDIR/$SERVERNAME/index.html
 done < hostlist.txt
-ls -l $SITESDIR; printf "/nDone!/n"
+printf "/nls $SITESDIR/n"; ls $SITESDIR; printf "/nDone!/n"
